48db6f52b25d918c4338b937aeafb6b390fd3026
Andreas Thune <andrthu@student.matnat.uio.no>
2016-02-05 16:04:19 +0100

    Description of changes.

---
+++ kap4/poisson.py
@@ -0,0 +1,60 @@
+from dolfin import *
+from numpy import zeros, sqrt, pi
+
+def Hp_sink(p,k):
+    return 0.5*(1-(k*pi)**(p+1))/(1-(k*pi))
+    
+
+def boundary(x,on_boundary): return on_boundary
+N = [100,1000,10000]
+K=[1,10,100]
+E = zeros((3,3))
+H2 = zeros((3,3))
+
+for i in range(len(N)):
+
+    
+
+    mesh = UnitIntervalMesh(N[i])
+    V = FunctionSpace(mesh,'Lagrange',1)
+    V2 = FunctionSpace(mesh,'Lagrange',1+3)
+    
+    bc = DirichletBC(V,Constant(0),boundary)
+    for j in range(len(K)):
+        f = Expression('(k*pi*pi*k)*sin(k*pi*x[0])',k=K[j])
+        ue1 = Expression('sin(k*pi*x[0])',k=K[j])
+        ue = interpolate(ue1,V2)
+
+        u = TrialFunction(V)
+        v = TestFunction(V)
+
+        a = inner(grad(u),grad(v))*dx
+        L = f*v*dx
+    
+        u=Function(V)
+        solve(a==L,u,bc)
+        
+        if i==2:
+            e=u-ue
+            A = assemble(e**2*dx)
+            B = assemble(inner(grad(e),grad(e))*dx)
+            print sqrt(A+B)
+            plot(e)
+            interactive()
+                        
+        
+        E[i,j] = errornorm(u,ue,'H1')
+        H2[i,j] = sqrt(Hp_sink(2,K[j]))
+
+C = zeros((3,3))
+
+C[0,:] = 100*E[0,:]/H2[0,:]
+C[1,:] = 1000*E[1,:]/H2[0,:]
+C[2,:] = 10000*E[2,:]/H2[0,:]
+
+print E
+print 
+print H2
+print
+print C
+        
