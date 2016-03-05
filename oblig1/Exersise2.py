a5451c0fbb16fb65553b13fa32f025bcf1d9389b
Andreas Thune <andrthu@student.matnat.uio.no>
2016-03-05 11:38:57 +0100

    add oblig exer 2

---
+++ oblig1/Exersise2.py
@@ -0,0 +1,82 @@
+from dolfin import *
+from numpy import pi,matrix, sqrt, diagflat,zeros,vstack,ones,log,array
+#from scipy.special import factorial as fac
+from math import factorial as fac
+from scipy import linalg
+
+
+
+
+
+def Dirichlet_boundary(x, on_boundary):
+    if on_boundary:
+        if x[0] == 0 or x[0] == 1:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+con = []
+
+
+for my in [1,0.001,0.000001]:
+
+    L2 = []
+    H1 = []
+    h_val = []
+
+    for h in [8,16,32,64]:
+
+                
+        mesh = UnitSquareMesh(h,h)
+        
+        V = FunctionSpace(mesh,'Lagrange',1)
+        V2 = FunctionSpace(mesh,'Lagrange',1+3)
+        
+        
+        f = Constant(0)
+        
+        ue = Expression('(1-exp(x[0]/%e))/(1-exp(1/%e))'%(my,my))
+        g = Expression('x[0]')
+            
+        
+        u = TrialFunction(V)
+        v = TestFunction(V)
+
+        MY = Constant(my)
+        
+        a = MY*inner(grad(u),grad(v))*dx +v*u.dx(0)*dx
+        L = f*v*dx 
+
+                
+                
+        bc = DirichletBC(V,g,Dirichlet_boundary)
+
+        U = Function(V)
+
+        solve(a==L,U,bc)
+        Ue = interpolate(ue,V2)
+        """
+        print
+        print errornorm(U,Ue,'H1')
+        print errornorm(U,Ue)
+        print my,h
+        print
+        """
+        L2.append(errornorm(U,Ue))
+        H1.append(errornorm(U,Ue,'H1'))
+        h_val.append( mesh.hmax())
+        
+        if my == 0.001 and h==64:
+            
+            print errornorm(U,Ue)
+            print assemble((U-Ue)**2*dx)
+            plot(U)
+            interactive()
+    if my == 1:
+        Q = vstack([log(array(h_val)),ones(len(h_val))]).T
+        con.append(linalg.lstsq(Q, log(array(L2)))[0])
+        con.append(linalg.lstsq(Q, log(array(H1)))[0])
+
+print con
