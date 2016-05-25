56c1767f41d8a4e335833f1d87a4921309e8b06e
Andreas Thune <andrthu@student.matnat.uio.no>
2016-05-25 16:11:43 +0200

    failed to add jacoboi PC case

--- kap8/testCG.py
+++ kap8/testCG.py
@@ -6,9 +6,9 @@ from numpy import array
 N = [16,32,64,128,256,512]
 
 M=[UnitIntervalMesh,UnitSquareMesh]
-Solver=[["cg"],["cg","amg"],["cg","ilu"]]
+Solver=[["cg"],["cg","default"],["cg","amg"],["cg","ilu"]]
 
-
+list_krylov_solver_preconditioners()
 
 def solving_time(A,b,solver):
     U = Function(V)
@@ -23,9 +23,10 @@ def solving_time(A,b,solver):
     
     return t1-t0
 
-cg_time = [[],[]]
-amg_time = [[],[]]
-ilu_time = [[],[]]
+cg_time     = [[],[]]
+amg_time    = [[],[]]
+ilu_time    = [[],[]]
+jacobi_time = [[],[]]
 for i in range(len(M)):
 
     
@@ -57,26 +58,38 @@ for i in range(len(M)):
         t2=solving_time(A,b,Solver[0])
         print "cg",t2
         cg_time[i].append(t2)
-
-        t2=solving_time(A,b,Solver[1])
+        try:
+            t2=solving_time(A,b,Solver[1])
+            print "jacobi",t2
+            jacobi_time[i].append(t2)
+        except:
+            jacobi_time[i].append(0)
+            
+        t2=solving_time(A,b,Solver[2])
         print "amg",t2
         amg_time[i].append(t2)
-        
-        t2=solving_time(A,b,Solver[2])
+
+        t2=solving_time(A,b,Solver[3])
         print "ilu",t2
         ilu_time[i].append(t2)
 
 plt.plot(array(N),array(cg_time[0]))
 plt.plot(array(N),array(amg_time[0]))
 plt.plot(array(N),array(ilu_time[0]))
-plt.legend(['cg','amg','ilu'])
+plt.plot(array(N),array(jacobi_time[0]))
+
+plt.legend(['cg','amg','ilu','jacobi'])
 plt.xlabel('dofs')
 plt.ylabel('time in seconds')
 plt.show()
+
+
 plt.plot(array(N)**2,array(cg_time[1]))
 plt.plot(array(N)**2,array(amg_time[1]))
 plt.plot(array(N)**2,array(ilu_time[1]))
-plt.legend(['cg','amg','ilu'])
+plt.plot(array(N)**2,array(jacobi_time[1]))
+
+plt.legend(['cg','amg','ilu','jacobi'])
 plt.xlabel('dofs')
 plt.ylabel('time in seconds')
 plt.show()
