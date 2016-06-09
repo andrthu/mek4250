87e2101d9475dc40516fb6b4fe847f7a864f14a8
Andreas Thune <andrthu@student.matnat.uio.no>
2016-06-09 16:24:56 +0200

    added non penalty solution in the penalty test

--- adjoint/my_bfgs/test_penalty.py
+++ adjoint/my_bfgs/test_penalty.py
@@ -4,7 +4,7 @@ import numpy as np
 from matplotlib.pyplot import *
 from scipy.integrate import trapz
 from scipy.optimize import minimize
-
+import test_serial as ser
 
 #n number of points, m number of intervalls
 def partition_func(n,m):
@@ -100,8 +100,18 @@ def mini_solver(y0,a,T,yT,n,m,my_list):
     t=np.linspace(0,T,n+1)
     #initial guess for control and penalty control is set to be 0
     x0 = SimpleVector(np.zeros(n+m))
-
     
+    dt = t[1]-t[0]
+
+    def ser_J(u):
+        return ser.Func(u,a,y0,yT,T)
+
+    def ser_grad_J(u):
+        l = ser.adjoint_solver(y0,a,len(u)-1,u,T,yT)
+        return ser.L2_grad(u,l,dt)
+
+    res1 = minimize(ser_J,np.zeros(n+1),method='L-BFGS-B', jac=ser_grad_J,
+                   options={'gtol': 1e-6, 'disp': False})
     
     H = None
     
@@ -118,7 +128,7 @@ def mini_solver(y0,a,T,yT,n,m,my_list):
             l,L = adjoint_solver(y0,a,n,m,u[:n+1],u[n+1:],T,yT,my_list[k])
             g = np.zeros(len(u))
             
-            g[:n+1]=float(T)*(u[:n+1]+L)/n
+            g[:n+1]=dt*(u[:n+1]+L)
 
             for i in range(m-1):
                 g[n+1+i]=l[i+1][0]-l[i][-1]
@@ -152,7 +162,7 @@ def mini_solver(y0,a,T,yT,n,m,my_list):
             S2 = SimpleVector(np.zeros(len(u)))
             return MuVector([Y1,Y2]),MuVector([S1,S2])
 
-            #return u1,l1,du1,ADJ1,STA1
+            
             
             
             
@@ -178,20 +188,23 @@ def mini_solver(y0,a,T,yT,n,m,my_list):
         mem_limit = 10
         options = {"mu_val": my_list[k], "old_hessian": None, 
                    "return_data": True,"mem_lim":mem_limit, "beta":1,
-                   "save_number":-1,}
+                   "save_number":-1,"jtol" : 1e-6,}
         
+        options2={"mem_lim" : mem_limit,"return_data": True,"jtol" : 1e-6,}
         
         S = MuLbfgs(J,grad_J,x0,Mud_J,Hinit=None,lam0=None,options=options)
-        #S = Lbfgs(J,grad_J,x0,options={"mem_lim" : mem_limit,"return_data": True,})
+        #S = Lbfgs(J,grad_J,x0,options=options2)
         data = S.solve()
         
         x0 = data['control']
         H = data['lbfgs']
-        #solver(y0,a,n,m,u,lam,T)
+        
         plot(t,x0.array()[:n+1])
+        plot(t,res1.x)
+        legend(["mu","normal"])
         print data['iteration']
         show()
-    print x0.array()
+    
 
 
 
@@ -206,10 +219,11 @@ if __name__ == "__main__":
     a = 1
     T = 1
     yT = 1
-    n = 100
+    n = 2000
     m = 10
 
-    my_list = [0.1,10,20,50]
+    my_list = [0.1,10,20,100,500]
+    #my_list = [500,800,900,1000]
     
     mini_solver(y0,a,T,yT,n,m,my_list)
 
