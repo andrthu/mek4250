c3cab9b4a1906beea384ef10d72125690cc58ae8
Andreas Thune <andrthu@student.matnat.uio.no>
2016-06-16 13:37:01 +0200

    add memory limit tests

---
+++ adjoint/my_bfgs/test_memory_limit.py
@@ -0,0 +1,132 @@
+from my_vector import *
+from lbfgs import Lbfgs,MuLbfgs
+import numpy as np
+from matplotlib.pyplot import *
+from scipy.integrate import trapz
+from scipy.optimize import minimize
+import test_serial as ser
+import test_penalty as pen
+
+
+def mini_solver(y0,a,T,yT,n,m,my_list,mem_limit,show_output=False):
+
+
+    t=np.linspace(0,T,n+1)
+    #initial guess for control and penalty control is set to be 0
+    x0 = SimpleVector(np.zeros(n+m))
+    
+    dt = t[1]-t[0]
+
+    def ser_J(u):
+        return ser.Func(u,a,y0,yT,T)
+
+    def ser_grad_J(u):
+        l = ser.adjoint_solver(y0,a,len(u)-1,u,T,yT)
+        return ser.L2_grad(u,l,dt)
+
+    res1 = minimize(ser_J,np.zeros(n+1),method='L-BFGS-B', jac=ser_grad_J,
+                   options={'gtol': 1e-6, 'disp': False})
+    res2 = []
+    
+    for k in range(len(my_list)):
+        #define reduced functional dependent only on u
+        def J(u):
+            y,Y=pen.solver(y0,a,n,m,u[:n+1],u[n+1:],T)
+            return pen.Functional2(y,u[:n+1],u[n+1:],yT,T,my_list[k])
+        
+        #define our gradient using by solving adjoint equation
+        def grad_J(u):
+            #adjoint_solver(y0,a,n,m,u,lam,T,yT,my)
+            l,L = pen.adjoint_solver(y0,a,n,m,u[:n+1],u[n+1:],T,yT,my_list[k])
+            g = np.zeros(len(u))
+            
+            g[:n+1]=dt*(u[:n+1]+L)
+
+            for i in range(m-1):
+                g[n+1+i]=l[i+1][0]-l[i][-1]
+                
+            return g
+
+
+
+        options={"mem_lim" : mem_limit,"return_data": True,"jtol" : 1e-4,}
+        try:
+            S = Lbfgs(J,grad_J,x0,options=options)
+
+            data = S.solve()
+        except Warning:
+            data = {'control':x0,'iteration':-1,}
+            
+        res2.append(data)
+        
+    return res1,res2
+
+
+def test_mem():
+
+
+    y0 = 1
+    T  = 1
+    a  = 1
+    yT = 10
+
+    M = [2,5,10,15,20,30]
+
+    N = 1000
+    t = np.linspace(0,T,N+1)
+
+    num_it = []
+
+    mul= [1,2]
+    for i in range(len(M)):
+
+        l = []
+        #figure()
+        for j in mul:
+            my = 500
+            res1,res2 = mini_solver(y0,a,T,yT,N,M[i],[my],int(np.floor(j*M[i])))
+
+            print M[i],j,res2[0]['iteration'],res1.nit
+            l.append(res2[0]['iteration'])
+        num_it.append(l)
+        
+        """
+            if j==1:
+                plot(t,res1.x)
+            plot(t,res2[0]['control'][:N+1])
+        show()
+        """
+
+    for i in range(len(M)):
+        print "--------------m=%d--------------" %(M[i])
+        for j in range(len(mul)):
+            print "|lbfgs memory=%d| #iterations=%d| #iterations/m=%.2f"%(mul[j]*M[i],num_it[i][j],num_it[i][j]/float(M[i]))
+        """
+        print "|lbfgs memory=%d| #iterations=%d| #iterations/m=%.2f"%(2*M[i],num_it[i][1],num_it[i][1]/float(M[i]))
+        """
+
+def test_const_NM_rate():
+    y0 = 1
+    T  = 1
+    a  = 1
+    yT = 10
+
+    M = [2,5,10,20]
+
+    for i in range(len(M)):
+
+        N = M[i]*200
+        my =0.5*N
+        t =np.linspace(0,T,N+1)
+        res1,res2 = mini_solver(y0,a,T,yT,N,M[i],[my],2*M[i])
+
+        print M[i],res2[0]['iteration']
+        figure()
+        plot(t,res1.x)
+        plot(t,res2[0]['control'][:N+1])
+        show()
+           
+if __name__ == "__main__":
+
+    #test_mem()
+    test_const_NM_rate()       
--- adjoint/my_bfgs/test_penalty.py
+++ adjoint/my_bfgs/test_penalty.py
@@ -98,7 +98,7 @@ def L2error(u,v,t):
     x = (u-v)**2
     return trapz(x,t)
 
-def mini_solver(y0,a,T,yT,n,m,my_list,show_output=False):
+def mini_solver(y0,a,T,yT,n,m,my_list,show_output=False,mem_limit=15):
     
     t=np.linspace(0,T,n+1)
     #initial guess for control and penalty control is set to be 0
@@ -190,7 +190,7 @@ def mini_solver(y0,a,T,yT,n,m,my_list,show_output=False):
                    "penaly_number"          : 1,
                    "return_data"            : False, }
         """
-        mem_limit = 10
+        #mem_limit = 10
         options = {"mu_val": my_list[k], "old_hessian": H, 
                    "return_data": True,"mem_lim":mem_limit, "beta":1,
                    "save_number":-1,"jtol" : 1e-4,}
@@ -296,6 +296,17 @@ def test_mu_values():
         for j in range(len(N)):
             print "error for M=%d and N=%d" % (M[i],N[j]) 
             print "| %.1e| %.1e| %.1e| %.1e| %.1e" %(Error2[i][j][0],Error2[i][j][1],Error2[i][j][2],Error2[i][j][3],Error2[i][j][4])
+
+
+    for i in range(len(M)):
+        print "----------------------------"
+        for j in range(len(N)):
+            print "error for M=%d and N=%d" % (M[i],N[j]) 
+            print "| %.1e| %.1e| %.1e| %.1e|" %(Error1[i][j][1]/Error1[i][j][0],Error1[i][j][2]/Error1[i][j][1],Error1[i][j][3]/Error1[i][j][2],Error1[i][j][4]/Error1[i][j][3])
+        print "----------------------------"
+        for j in range(len(N)):
+            print "error for M=%d and N=%d" % (M[i],N[j]) 
+            print "| %.1e| %.1e| %.1e| %.1e|" %(Error2[i][j][1]/Error2[i][j][0],Error2[i][j][2]/Error2[i][j][1],Error2[i][j][3]/Error2[i][j][2],Error2[i][j][4]/Error2[i][j][3])
                                                      
     
     for i in range(len(M)):
@@ -316,7 +327,30 @@ def test_mu_values():
             show()
 
     
-            
+def test_MuLbfgs():
+
+
+    y0 = 1
+    T = 1
+    yT = 1
+    a = 1
+
+    N = [2000,3000]
+    m = 10
+
+    for i in range(len(N)):
+        MY = [0.1*N[i],0.5*N[i]]
+        t = np.linspace(0,T,N[i]+1)
+        
+        res1,res2,res3 = mini_solver(y0,a,T,yT,N[i],m,MY)
+
+        figure()
+        plot(t,res1.x)
+        for j in range(len(MY)):
+            plot(t,res2[j]['control'][:N[i]+1])
+            print res2[j]['iteration'],res3[j]['iteration']
+        show()
+    
 if __name__ == "__main__":
 
     
@@ -332,4 +366,185 @@ if __name__ == "__main__":
     
     #mini_solver(y0,a,T,yT,n,m,my_list,show_output=True)
 
-    test_mu_values()
+    #test_mu_values()
+    test_MuLbfgs()
+
+"""
+terminal>> python test_mu_values()
+----------------------------
+error for M=2 and N=100
+| 7.1e-03| 5.4e-01| 5.3e-04| 5.4e-01| 5.4e-01
+error for M=2 and N=300
+| 9.2e-04| 2.5e-04| 5.4e-01| 1.0e-05| 5.4e-01
+error for M=2 and N=500
+| 3.4e-04| 8.8e-04| 2.3e-05| 5.4e-01| 2.7e-06
+error for M=2 and N=900
+| 1.1e-04| 7.6e-04| 7.4e-06| 1.1e-06| 4.2e-07
+error for M=2 and N=1000
+| 8.8e-05| 7.3e-04| 2.6e-03| 9.9e-07| 3.8e-07
+error for M=2 and N=2000
+| 2.2e-05| 5.8e-04| 2.7e-04| 4.0e-04| 8.1e-04
+----------------------------
+error for M=2 and N=100
+| 7.1e-03| 2.0e-03| 5.4e-04| 1.1e-04| 4.2e-05
+error for M=2 and N=300
+| 9.2e-04| 2.4e-04| 6.3e-05| 1.2e-05| 4.7e-06
+error for M=2 and N=500
+| 3.4e-04| 8.8e-05| 2.3e-05| 4.5e-06| 1.7e-06
+error for M=2 and N=900
+| 1.1e-04| 2.8e-05| 7.0e-06| 1.4e-06| 5.2e-07
+error for M=2 and N=1000
+| 8.8e-05| 2.2e-05| 5.7e-06| 1.1e-06| 4.2e-07
+error for M=2 and N=2000
+| 2.2e-05| 5.6e-06| 1.6e-06| 2.8e-07| 1.0e-07
+----------------------------
+error for M=5 and N=100
+| 7.2e-02| 5.4e-01| 5.4e-01| 5.4e-01| 5.4e-01
+error for M=5 and N=300
+| 1.4e-02| 4.1e-03| 5.4e-01| 5.4e-01| 5.4e-01
+error for M=5 and N=500
+| 5.7e-03| 5.4e-01| 4.7e-04| 9.4e-05| 5.4e-01
+error for M=5 and N=900
+| 1.9e-03| 5.4e-01| 5.4e-01| 5.4e-01| 5.4e-01
+error for M=5 and N=1000
+| 1.5e-03| 5.4e-01| 1.3e-03| 4.0e-05| 5.4e-01
+error for M=5 and N=2000
+| 4.0e-04| 5.4e-01| 1.2e-03| 2.5e-05| 5.4e-01
+----------------------------
+error for M=5 and N=100
+| 7.2e-02| 2.7e-02| 8.5e-03| 5.4e-01| 6.9e-04
+error for M=5 and N=300
+| 1.4e-02| 4.1e-03| 1.1e-03| 1.9e-04| 7.1e-05
+error for M=5 and N=500
+| 5.7e-03| 1.6e-03| 4.0e-04| 5.7e-05| 2.9e-05
+error for M=5 and N=900
+| 1.9e-03| 4.9e-04| 1.3e-04| 2.2e-05| 9.5e-06
+error for M=5 and N=1000
+| 1.5e-03| 3.9e-04| 1.0e-04| 1.8e-05| 7.7e-06
+error for M=5 and N=2000
+| 4.0e-04| 9.9e-05| 2.6e-05| 4.6e-06| 1.9e-06
+----------------------------
+error for M=10 and N=100
+| 1.8e-01| 5.4e-01| 5.4e-01| 5.4e-01| 5.4e-01
+error for M=10 and N=300
+| 5.1e-02| 1.8e-02| 5.3e-03| 5.4e-01| 5.4e-01
+error for M=10 and N=500
+| 2.4e-02| 5.4e-01| 5.4e-01| 5.4e-01| 5.4e-01
+error for M=10 and N=900
+| 8.9e-03| 2.8e-03| 1.0e-03| 1.7e-03| 5.0e-04
+error for M=10 and N=1000
+| 7.4e-03| 5.4e-01| 5.4e-01| 5.4e-01| 5.4e-01
+error for M=10 and N=2000
+| 2.1e-03| 2.4e-03| 9.4e-04| 5.1e-04| 3.0e-04
+----------------------------
+error for M=10 and N=100
+| 1.8e-01| 5.4e-01| 5.4e-01| 7.4e-03| 5.4e-01
+error for M=10 and N=300
+| 5.1e-02| 1.8e-02| 5.4e-03| 1.0e-03| 4.3e-04
+error for M=10 and N=500
+| 2.4e-02| 7.4e-03| 2.1e-03| 3.7e-04| 1.6e-04
+error for M=10 and N=900
+| 8.9e-03| 2.5e-03| 6.8e-04| 1.2e-04| 4.9e-05
+error for M=10 and N=1000
+| 7.4e-03| 2.1e-03| 5.6e-04| 9.6e-05| 4.0e-05
+error for M=10 and N=2000
+| 2.1e-03| 5.5e-04| 1.4e-04| 2.4e-05| 1.0e-05
+----------------------------
+error for M=2 and N=100
+| 7.6e+01| 9.9e-04| 1.0e+03| 1.0e+00|
+error for M=2 and N=300
+| 2.7e-01| 2.2e+03| 1.9e-05| 5.4e+04|
+error for M=2 and N=500
+| 2.6e+00| 2.6e-02| 2.4e+04| 5.1e-06|
+error for M=2 and N=900
+| 7.0e+00| 9.8e-03| 1.5e-01| 3.8e-01|
+error for M=2 and N=1000
+| 8.3e+00| 3.5e+00| 3.9e-04| 3.9e-01|
+error for M=2 and N=2000
+| 2.6e+01| 4.6e-01| 1.5e+00| 2.0e+00|
+----------------------------
+error for M=2 and N=100
+| 2.8e-01| 2.7e-01| 2.0e-01| 3.9e-01|
+error for M=2 and N=300
+| 2.6e-01| 2.6e-01| 2.0e-01| 3.8e-01|
+error for M=2 and N=500
+| 2.6e-01| 2.6e-01| 2.0e-01| 3.8e-01|
+error for M=2 and N=900
+| 2.5e-01| 2.6e-01| 2.0e-01| 3.8e-01|
+error for M=2 and N=1000
+| 2.5e-01| 2.6e-01| 2.0e-01| 3.7e-01|
+error for M=2 and N=2000
+| 2.5e-01| 2.9e-01| 1.7e-01| 3.7e-01|
+----------------------------
+error for M=5 and N=100
+| 7.6e+00| 1.0e+00| 1.0e+00| 1.0e+00|
+error for M=5 and N=300
+| 3.0e-01| 1.3e+02| 1.0e+00| 1.0e+00|
+error for M=5 and N=500
+| 9.5e+01| 8.7e-04| 2.0e-01| 5.7e+03|
+error for M=5 and N=900
+| 2.8e+02| 1.0e+00| 1.0e+00| 1.0e+00|
+error for M=5 and N=1000
+| 3.5e+02| 2.5e-03| 3.0e-02| 1.3e+04|
+error for M=5 and N=2000
+| 1.4e+03| 2.3e-03| 2.0e-02| 2.2e+04|
+----------------------------
+error for M=5 and N=100
+| 3.7e-01| 3.2e-01| 6.4e+01| 1.3e-03|
+error for M=5 and N=300
+| 3.0e-01| 2.7e-01| 1.7e-01| 3.7e-01|
+error for M=5 and N=500
+| 2.8e-01| 2.5e-01| 1.5e-01| 5.0e-01|
+error for M=5 and N=900
+| 2.6e-01| 2.6e-01| 1.8e-01| 4.2e-01|
+error for M=5 and N=1000
+| 2.5e-01| 2.6e-01| 1.8e-01| 4.2e-01|
+error for M=5 and N=2000
+| 2.5e-01| 2.6e-01| 1.8e-01| 4.1e-01|
+----------------------------
+error for M=10 and N=100
+| 3.1e+00| 1.0e+00| 1.0e+00| 1.0e+00|
+error for M=10 and N=300
+| 3.5e-01| 3.0e-01| 1.0e+02| 1.0e+00|
+error for M=10 and N=500
+| 2.3e+01| 1.0e+00| 1.0e+00| 1.0e+00|
+error for M=10 and N=900
+| 3.2e-01| 3.5e-01| 1.7e+00| 2.9e-01|
+error for M=10 and N=1000
+| 7.3e+01| 1.0e+00| 1.0e+00| 1.0e+00|
+error for M=10 and N=2000
+| 1.1e+00| 4.0e-01| 5.4e-01| 5.9e-01|
+----------------------------
+error for M=10 and N=100
+| 3.1e+00| 1.0e+00| 1.4e-02| 7.3e+01|
+error for M=10 and N=300
+| 3.5e-01| 3.0e-01| 1.9e-01| 4.3e-01|
+error for M=10 and N=500
+| 3.1e-01| 2.8e-01| 1.8e-01| 4.2e-01|
+error for M=10 and N=900
+| 2.8e-01| 2.8e-01| 1.7e-01| 4.2e-01|
+error for M=10 and N=1000
+| 2.8e-01| 2.7e-01| 1.7e-01| 4.2e-01|
+error for M=10 and N=2000
+| 2.6e-01| 2.5e-01| 1.7e-01| 4.1e-01|
+
+[[2], [2], [2], [2], [2], [2]]
+
+[[5, -1, 8, -1, -1], [5, 5, -1, 5, -1], [4, 3, 6, -1, 8], [4, 3, 7, 5, 6], [4, 3, 3, 9, 5], [4, 3, 3, 3, 3]]
+
+[[5, 5, 4, 3, 3], [5, 4, 4, 3, 3], [4, 4, 4, 3, 3], [4, 4, 4, 3, 3], [4, 4, 4, 3, 3], [4, 4, 3, 3, 3]]
+
+[[2], [2], [2], [2], [2], [2]]
+
+[[7, -1, -1, -1, -1], [8, 16, -1, -1, -1], [8, -1, 6, 21, -1], [8, -1, -1, -1, -1], [8, -1, 7, 9, -1], [9, -1, 6, 20, -1]]
+
+[[7, 8, 8, -1, 10], [8, 8, 9, 10, 10], [8, 9, 9, 10, 14], [8, 9, 10, 15, 15], [8, 9, 10, 15, 15], [9, 10, 11, 16, 16]]
+
+[[2], [2], [2], [2], [2], [2]]
+
+[[20, -1, -1, -1, -1], [22, 11, 22, -1, -1], [22, -1, -1, -1, -1], [22, 11, 30, 31, 22], [22, -1, -1, -1, -1], [21, 11, 11, 11, 11]]
+
+[[20, -1, -1, 34, -1], [22, 22, 22, 23, 22], [22, 22, 21, 22, 22], [22, 20, 22, 22, 22], [22, 21, 22, 22, 22], [21, 21, 21, 22, 22]]
+
+
+"""
