562c2609106f09bf9431d1664cdf3d9b316135d7
Andreas Thune <andrthu@student.matnat.uio.no>
2016-06-22 12:03:00 +0200

    tested optimalControl class, and fixed its options

--- adjoint/optimalContolProblem.py
+++ adjoint/optimalContolProblem.py
@@ -1,16 +1,18 @@
 from my_bfgs.lbfgs import Lbfgs
+from my_bfgs.my_vector import SimpleVector
 from penalty import partition_func
+from scipy.integrate import trapz
 import numpy as np
 
 class OptimalControlProblem():
 
-    def __init__(self,y0,yT,T,J,grad_J,options=None):
+    def __init__(self,y0,yT,T,J,grad_J,Lbfgs_options=None,options=None):
         """
         Initialize OptimalControlProblem
 
         Valid options are:
         - T: End time 
-        - J: Functional depending on u and y
+        - J: Functional depending on u and y and dt
         - options: More options
         """
         self.T      = T
@@ -19,24 +21,34 @@ class OptimalControlProblem():
         self.J      = J
         self.grad_J = grad_J
         
-        self.set_options(options)
+        self.set_options(options,Lbfgs_options)
+        
+        self.Vec = self.options['Vector']
 
-    def set_options(self,user_options):
+    def set_options(self,user_options,user_Lbfgs):
         """
         Method for setting options
         """
         options = self.default_options()
+        Lbfgs_options = self.default_Lbfgs_options()
         
         if user_options!=None:
             for key, val in user_options.iteritems():
                 options[key]=val
-        self.options = options
 
-    def default_options(self):
+        if user_Lbfgs!=None:
+            for key, val in user_Lbfgs.iteritems():
+                Lbfgs_options[key]=val
+
+        
+        self.options = options
+        self.Lbfgs_options = Lbfgs_options
+
+    def default_Lbfgs_options(self):
         
         default = {"jtol"                   : 1e-4,
                    "maxiter"                : 200,
-                   "mem_lim"                : 5,
+                   "mem_lim"                : 10,
                    "Vector"                 : SimpleVector,
                    "Hinit"                  : "default",
                    "beta"                   : 1,
@@ -44,13 +56,17 @@ class OptimalControlProblem():
 
         return default
 
-    
+    def default_options(self):
+
+        default = {"Vector" : SimpleVector,
+                   "Lbfgs"  : Lbfgs,}
+        return default
         
         
-    def ODE_update(self,y,u,i,j,dt)
+    def ODE_update(self,y,u,i,j,dt):
         raise NotImplementedError,'ODE_update not implemented'
 
-    def adjoint_update(self,l,i,dt)
+    def adjoint_update(self,l,i,dt):
         raise NotImplementedError,'adjoint_update not implemented'
     
     def ODE_solver(self,u,N):
@@ -69,33 +85,7 @@ class OptimalControlProblem():
 
     
         return y
-    """
-        else:
 
-            
-            y = partition_func(N+1,m)
-
-            y[0][0]=y0
-            for i in range(1,m):
-            y[i][0] = u[N+i]
-
-            start=1
-            for i in range(m):        
-                for j in range(len(y[i])-1):
-                    y[i][j+1] = self.ODE_update(y[i],u,j,start+j)
-                start = start + len(y[i]) - 1
-            y_ret.append(y)
-
-            Y = np.zeros(N+1)
-            start = 0
-            for i in range(m):
-                Y[start:start+len(y[i])-1] = y[i][:-1]
-                start = start + len(y[i]) - 1
-            Y[-1] = y[-1][-1]
-            
-            y_ret.append(Y)
-        return y_ret
-    """
     def ODE_penalty_solver(self,u,N,m):
         
         T = self.T
@@ -107,7 +97,7 @@ class OptimalControlProblem():
         for i in range(1,m):
             y[i][0] = u[N+i]
 
-        start=1
+        start=0
         for i in range(m):        
             for j in range(len(y[i])-1):
                 y[i][j+1] = self.ODE_update(y[i],u,j,start+j,dt)
@@ -132,41 +122,13 @@ class OptimalControlProblem():
 
         y = self.ODE_solver(u,N)
 
-        l=np.zeros(n+1)
+        l=np.zeros(N+1)
         
         l[-1] = y[-1] -yT
         for i in range(N):
             l[-(i+2)] = self.adjoint_update(l,i,dt)
              
         return l
-    """
-        
-        else:
-
-            l = partition_func(N+1,m)
-            y,Y = self.ODE_solver(u,N,m)
-
-            
-            l[-1][-1] = y[-1][-1] - yT
-            for i in range(m-1):
-                l[i][-1]=my*(y[i][-1]-u[N+1+i])
-
-            for i in range(m):
-                for j in range(len(l[i])-1):
-                    l[i][-(j+2)] = self.adjoint_update(l[i],j,dt)
-            l_ret.append(l)
-            L=zeros(N+1)
-            
-            start=0
-            for i in range(m):
-                L[start:start+len(l[i])-1] = l[i][:-1]
-                start = start + len(l[i])-1
-            L[-1]=l[-1][-1]
-            
-            l_ret.append(L)
-            
-        return l_ret
-    """
     
     def adjoint_penalty_solver(self,u,N,m,my):
 
@@ -187,7 +149,7 @@ class OptimalControlProblem():
             for j in range(len(l[i])-1):
                 l[i][-(j+2)] = self.adjoint_update(l[i],j,dt)
             
-        L=zeros(N+1)
+        L=np.zeros(N+1)
             
         start=0
         for i in range(m):
@@ -200,18 +162,18 @@ class OptimalControlProblem():
         
     def Functional(self,u,N):
 
-        return self.J(u,self.ODE_solver(u,N)[-1],self.T)
+        return self.J(u,self.ODE_solver(u,N)[-1],self.yT,self.T)
 
     def Penalty_Functional(self,u,N,m,my):
 
         y,Y = self.ODE_penalty_solver(u,N,m)
 
-        J_val = self.J(u[:N+1],y[-1][-1],self.T)
+        J_val = self.J(u[:N+1],y[-1][-1],self.yT,self.T)
 
         penalty = 0
 
         for i in range(m-1):
-            penalty = penalty + my*((y[i][-1]-u[N+i])**2)
+            penalty = penalty + my*((y[i][-1]-u[N+1+i])**2)
     
         return J_val + 0.5*penalty
 
@@ -221,9 +183,9 @@ class OptimalControlProblem():
     def solve(self,N,x0=None,Lbfgs_options=None):
 
         
-        dt=float(T)/n
+        dt=float(T)/N
         if x0==None:
-            x0 = np.zeros(N+1)
+            x0 = self.Vec(np.zeros(N+1))
             
         def J(u):
             return self.Functional(u,N)
@@ -233,14 +195,14 @@ class OptimalControlProblem():
             return self.grad_J(u,l,dt)
 
         if Lbfgs_options==None:
-            Loptions = self.options
+            Loptions = self.Lbfgs_options
         else:
-            Loptions = self.options
+            Loptions = self.Lbfgs_options
             for key, val in Lbfgs_options.iteritems():
                 Loptions[key]=val
             
         
-        solver = Lbfgs(x0,J,grad_J,options=Loptions)
+        solver = Lbfgs(J,grad_J,x0,options=Loptions)
 
         res = solver.solve()
 
@@ -250,38 +212,38 @@ class OptimalControlProblem():
     def penalty_solve(self,N,m,my_list,x0=None,Lbfgs_options=None):
 
 
-        dt=float(T)/n
+        dt=float(T)/N
         if x0==None:
-            x0 = np.zeros(N+m)
+            x0 = self.Vec(np.zeros(N+m))
         x = None
         
         for i in range(len(my_list)):
         
             def J(u):                
-                return Penalty_Functional(u,N,m,my_list[i])
+                return self.Penalty_Functional(u,N,m,my_list[i])
 
             def grad_J(u):
 
-                l,L = adjoint_penalty_solver(u,N,m,my_list[i])
+                l,L = self.adjoint_penalty_solver(u,N,m,my_list[i])
 
-                g = zeros(len(u))
+                g = np.zeros(len(u))
 
                 g[:N+1]=self.grad_J(u[:N+1],L,dt)
 
-                for i in range(m-1):
-                    g[N+1+i]=l[i+1][0]-l[i][-1]
+                for j in range(m-1):
+                    g[N+1+j]=l[j+1][0]-l[j][-1]
                     
                 return g
             
             if Lbfgs_options==None:
-                Loptions = self.options
+                Loptions = self.Lbfgs_options
             else:
-                Loptions = self.options
+                Loptions = self.Lbfgs_options
                 for key, val in Lbfgs_options.iteritems():
                     Loptions[key]=val
 
             
-            solver = Lbfgs(x0,J,grad_J,options=Loptions)
+            solver = Lbfgs(J,grad_J,x0,options=Loptions)
             
             res = solver.solve()
             
@@ -290,10 +252,71 @@ class OptimalControlProblem():
 
         return res
 
+    
+
 class Problem1(OptimalControlProblem):
+    """
+    optimal control with ODE y=ay'+u
+    """
 
     def __init__(self,y0,yT,T,a,J,grad_J,options=None):
 
         OptimalControlProblem.__init__(self,y0,yT,T,J,grad_J,options)
 
         self.a = a
+
+
+    def ODE_update(self,y,u,i,j,dt):
+        a = self.a
+        return (y[i] +dt*u[j+1])/(1.-dt*a)
+
+
+    def adjoint_update(self,l,i,dt):
+
+        return (1+dt*a)*l[-(i+1)] 
+
+
+if __name__=="__main__":
+
+    from matplotlib.pyplot import *
+
+    y0 = 1
+    yT = 1
+    T  = 1
+    a  = 1
+
+    def J(u,y,yT,T):
+        t = np.linspace(0,T,len(u))
+
+        I = trapz(u**2,t)
+
+        return 0.5*(I + (y-yT)**2)
+
+    def grad_J(u,p,dt):
+        return dt*(u+p)
+
+
+    problem = Problem1(y0,yT,T,a,J,grad_J)
+
+    N = 1000
+    m = 10
+    
+    res1 = problem.solve(N)
+
+    t=np.linspace(0,T,N+1)
+    u1 = res1['control'].array()[:N+1]
+    figure()
+    plot(t,u1)
+    #show()
+
+
+    opt = {"mem_lim":20}
+    res2 = problem.penalty_solve(N,m,[500],Lbfgs_options=opt)
+
+    u2 = res2['control'].array()[:N+1]
+
+    plot(t,u2)
+    show()
+    
+
+    
