a1d0e6a57b47607d2434f944c752bb5a369d1502
Andreas Thune <andrthu@student.matnat.uio.no>
2016-07-07 17:15:20 +0200

    fenics opti

--- adjoint/dolfin_adj/simple.py
+++ adjoint/dolfin_adj/simple.py
@@ -13,9 +13,9 @@ def ODE_solver(y0,N,U,show_plot=False):
     y = TrialFunction(V)
     v = TestFunction(V)
 
-    #u = Function(V)
-    #u.vector().array()[:]=U[:]
-    u = interpolate(U,V)
+    u = Function(V)
+    u.vector()[:]=U.copy()[:]
+    #u = interpolate(U,V)
 
     a = (y.dx(0)*v - v*y)*dx
     L = u*v*dx
@@ -31,12 +31,12 @@ def ODE_solver(y0,N,U,show_plot=False):
         interactive()
     return Y
 
-def adjoint_ODE_solver(y0,yT,N,u):
+def adjoint_ODE_solver(y0,yT,N,u,show_plot=False):
     
     mesh = UnitIntervalMesh(N)
 
     V = FunctionSpace(mesh,'CG',1)
-    y = ODE_solver(y0,N,u)
+    y = ODE_solver(y0,N,u.copy())
 
     l = TrialFunction(V)
     v = TestFunction(V)
@@ -50,24 +50,31 @@ def adjoint_ODE_solver(y0,yT,N,u):
     lam = Function(V)
 
     solve(a==L,lam,bc)
-
-    plot(lam)
-    interactive()
+    if show_plot:
+        plot(lam)
+        interactive()
+    return lam
     
 
 def J_Func(u,y0,yT,N):
     
     y = ODE_solver(y0,N,u,show_plot=False)
 
-    A = assemble(u**2*dx)
+    
+    mesh = UnitIntervalMesh(N)
 
-    return 0.5*(A + (y.vector().array()[0]-yT)**2)
+    V = FunctionSpace(mesh,'CG',1)
+    U = Function(V)
+    U.vector()[:]=u.copy()[:]
+    A = assemble(U**2*dx)
+    return 0.5*A + 0.5*(y.vector().array()[0]-yT)**2
 
 def opti(y0,yT,N):
 
     mesh = UnitIntervalMesh(N)
+    V = FunctionSpace(mesh,'CG',1)
     dt = mesh.hmax()
-    
+    print dt,'loool'
     def J(u):
         return J_Func(u,y0,yT,N)
 
@@ -75,17 +82,64 @@ def opti(y0,yT,N):
 
         lam = adjoint_ODE_solver(y0,yT,N,u)
 
-        gr = u + lam
-
-        return gr.vector().array()*dt
+        gr = u.copy() + lam.vector().array().copy()
         
+        return gr*dt
+
+
+    res = minimize(J,np.zeros(N+1),method='L-BFGS-B', jac=grad_J,
+               options={'gtol': 1e-6, 'disp': True})
+
+    
+    y = ODE_solver(y0,N,res.x,show_plot=True)
+    print res.x
+    x = Function(V)
+    x.vector()[:] = res.x[:]
+    plot(x)
+    interactive()
+
+def finite_diff(u,y0,yT,N,J):
+
+    eps = 1./100
+
+    grad_J = np.zeros(len(u))
+
+    for i in range(len(u)):
+        e = np.zeros(len(u))
+        e[i]=eps
+        J1 = J(u,y0,yT,N)
+        J2 = J(u+e,y0,yT,N)        
+        grad_J[i] = (J2-J1)/eps
+        print J1,J2
+
+    return grad_J
+
+ 
+    
 if __name__ == "__main__":
     
     y0=1
-    yT=1
-    N=100
-    #u = np.zeros(N+1)
-    u= Constant(0.0)
-    y = ODE_solver(y0,N,u,show_plot=True)
+    yT=10
+    N=10
+    u = np.zeros(N+1)
+    u[N/2]=1
+    #u= Constant(0.0)
+    #y = ODE_solver(y0,N,u,show_plot=True)
 
-    adjoint_ODE_solver(y0,yT,N,u)
+    #adjoint_ODE_solver(y0,yT,N,u,show_plot=True)
+
+    opti(y0,yT,N)
+
+    """
+    a = finite_diff(u,y0,yT,N,J_Func)
+
+    mesh = UnitIntervalMesh(N)
+
+    V = FunctionSpace(mesh,'CG',1)
+
+    A = Function(V)
+    A.vector()[:]=a
+    print a 
+    plot(A)
+    interactive()
+    """
