e20496a6de008e418d50f6c2e65fd9191b2d4311
andrthu <andrthu@student.matnat.uio.no>
2016-08-11 14:20:49 +0200

    made adjoint solver for burger

--- adjoint/dolfin_adj/burger.py
+++ adjoint/dolfin_adj/burger.py
@@ -3,12 +3,15 @@ from dolfin import *
 def Dt(u,u_,timestep):
     return (u-u_)/timestep
 
-def burger_solve(ic,start,end,V,Tn):
+def burger_solve(ic,start,end,V,Tn,show_plot=False):
 
     
     U = []
     u_ = ic.copy()
     U.append(u_.copy())
+    if show_plot:
+        plot(u_)
+        interactive()
 
     u = Function(V)
     v = TestFunction(V)
@@ -29,6 +32,9 @@ def burger_solve(ic,start,end,V,Tn):
         U.append(u_.copy())
 
         t += float(timestep)
+        if show_plot:
+            plot(u_)
+            interactive()
 
     return U
 
@@ -42,19 +48,63 @@ def J(U,T):
     return timestep*s
 
 def adjoint_burger_solve(ic,start,end,V,Tn):
-    return "nix"
+    
 
+    U = burger_solve(ic,start,end,V,Tn)
+    P = []
+    timestep = Constant((end-start)/float(Tn))
+
+    nu = Constant(0.0001)
+    
+    p_ = project(Constant(0.0),V)
+    P.append(p_.copy())
+
+    p = Function(V)
+    v = TestFunction(V)
+    
+    u = U[-1].copy()
+
+    F = (Dt(p,p_,timestep)*v + u*p.dx(0)*v -nu*p.dx(0)*v.dx(0) +u*v)*dx
+    
+    bc = DirichletBC(V,0.0,"on_boundary")
+
+    t = end
+    count = -2
+    while ( t> start + DOLFIN_EPS):
+
+        solve(F==0,p,bc)
+        p_.assign(p)
+        u.assign(U[count].copy())
+        
+        P.append(p_.copy())
+        count = count -1
+        t = t - float(timestep)
+    
+    return P
+    
 if __name__ == "__main__":
     
-    n = 10
-    Tn = 10
+    n = 40
+    Tn = 30
     start = 0
-    end = 1
+    end = 0.5
     mesh = UnitIntervalMesh(n)
     V = FunctionSpace(mesh,"CG",1)
 
-    ic = project(Expression("10*sin(2*pi*x[0])"),V)
+    ic = project(Expression("x[0]*(1-x[0])"),V)
 
-    U = burger_solve(ic,start,end,V,Tn)
+    U = burger_solve(ic,start,end,V,Tn,show_plot=False)
 
     print J(U,end-start)
+
+
+    P = adjoint_burger_solve(ic,start,end,V,Tn)
+    
+    for i in range(len(P)):
+        plot(P[i])
+        interactive()
+    """
+    
+    plot(ic + P[-1])
+    interactive()
+    """
