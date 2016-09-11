84802126babed6d0cea1ea193f7e1124178e1df2
Andreas Thune <andrthu@student.matnat.uio.no>
2016-09-11 15:26:14 +0200

    did some gather with numpy

--- adjoint/parallelPythonTest/test_comunication.py
+++ adjoint/parallelPythonTest/test_comunication.py
@@ -1,4 +1,6 @@
 from mpi4py import MPI
+import numpy as np
+
 
 comm = MPI.COMM_WORLD
 
@@ -7,14 +9,21 @@ rank = comm.Get_rank()
 
 if rank == 0:
 
-    data = [i for i in range(size)]
+    init = [10*(i+1) for i in range(size)]
+    
+    result = np.empty(10*size,dtype='i')
 else:
-    data = None
-data = comm.scatter(data,root=0)
+    init=None
+    result=None
+init = comm.scatter(init,root=0)
+print init,rank
 
-new_data = [data + i for i in range(3)]
+new_data = np.empty(10,dtype='i')
 
-result = comm.gather(new_data,root=0)
+for i in range(len(new_data)):
+    new_data[i] = init + i
+
+comm.Gather(new_data,result,root=0)
 if rank==0:
     print result
 
