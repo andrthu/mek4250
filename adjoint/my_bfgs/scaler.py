fixed scaling

adjoint/my_bfgs/scaler.py
@@ -13,8 +13,9 @@ class PenaltyScaler():
         
         grad = self.grad_J(self.x0)
         N = self.N
-        gamma = np.max(abs(grad[N+1:]))/np.max(abs(grad[:N+1]))
-        print gamma
+        #gamma = 20*np.max(abs(grad[:N+1]))/np.max(abs(grad[N+1:]))
+        gamma = 20*(self.m/(N+1.))*np.sum(abs(grad[:N+1]))/np.sum(abs(grad[N+1:]))
+        print gamma,np.max(abs(grad[N+1:])),np.min(abs(grad[N+1:]))
         return gamma
 
     def var(self,x):

adjoint/my_bfgs/steepest_decent.py
@@ -51,6 +51,7 @@ class SteepestDecent():
         self.decomp=decomp
         self.set_options(options)
         
+        
         scaler = self.scale_problem(scale,x0,J,grad_J)
 
         self.data = OptimizationControl(self.x0,self.J,
@@ -90,11 +91,12 @@ class SteepestDecent():
         print y0[N+1:]
         J_ = lambda x: J(scaler.func_var(x))
         grad_J_ = lambda x : scaler.grad(grad_J)(scaler.func_var(x))
-
+        print np.max(abs(grad_J(x0)[:N+1])),np.max(abs(grad_J(x0)[N+1:]))
+        print np.max(abs(grad_J_(y0)[:N+1])),np.max(abs(grad_J_(y0)[N+1:]))
         self.J = J_
         self.x0 = y0
         self.grad_J = grad_J_
-        
+        self.scale = True
         return scaler
         
 
@@ -160,7 +162,15 @@ class SteepestDecent():
             N = len(y)-self.decomp
             y = y[:N+1]
         """
-        if np.sqrt(np.sum(y**2)/len(y))<self.options['jtol']:
+        if self.data.scaler==None:
+            grad_norm = np.sqrt(np.sum(y**2)/len(y))
+        else:
+            N = self.data.scaler.N
+            gamma = self.data.scaler.gamma
+            grad_norm = np.sum(y[:N+1]**2)/(len(y))
+            grad_norm += np.sum(y[N+1:]**2)/(len(y)*gamma)
+            grad_norm = np.sqrt(grad_norm)
+        if grad_norm<self.options['jtol']:
             #print 'Success'
             #print np.sqrt(np.sum(y**2)/len(y))
             return 1

adjoint/test_SDpenalty_scaler.py
@@ -18,29 +18,36 @@ def grad_J(u,p,dt,alp):
 T =  1
 y0 = 1
 a =  1
-yT = 3
+yT = 1
 alpha = 0.5
 
 N = 500
-m = 10
-mu = 1
+m = 5
+mu = 20
 
-opt = {'maxiter':100}
+opt = {'maxiter':500}
 problem = Problem3(y0,yT,T,a,alpha,J,grad_J)
 res1=problem.penalty_solve(N,m,[mu],algorithm='my_steepest_decent')
 
 JJ,grad_JJ = problem.generate_reduced_penalty(1./500,N,m,mu)
-
-solver2 = SteepestDecent(JJ,grad_JJ,np.zeros(N+m)+1,
+x0 = np.zeros(N+m)
+x0[N+1:] = 1
+solver2 = SteepestDecent(JJ,grad_JJ,x0,
                          options=opt,scale={'m':m})
 res2 = solver2.solve()
 
-solver3 = SteepestDecent(JJ,grad_JJ,np.zeros(N+m)+1,options=opt)
+solver3 = SteepestDecent(JJ,grad_JJ,x0,options=opt)
 res3 = solver3.solve()
 print res1.niter,res2.niter,res3.niter
-
+plt.figure()
 plt.plot(res1.x[N+1:],'>r')
-plt.plot(res2.x[N+1:],'b--')
+#plt.plot(res2.x[N+1:],'b--')
 plt.plot(res3.x[N+1:])
-plt.plot(res2.x[N+1:]*res2.scaler.gamma)
+plt.plot(res2.x[N+1:]*res2.scaler.gamma,'b--')
+plt.show()
+
+plt.figure()
+plt.plot(res1.x[:N+1],'r')
+plt.plot(res2.x[:N+1],'b--')
+plt.plot(res3.x[:N+1])
 plt.show()