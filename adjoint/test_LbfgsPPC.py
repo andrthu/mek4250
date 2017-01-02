d19cd1e27bd29fd120683bcc87f307446f5abc17
andrthu <andrthu@student.matnat.uio.no>
2017-01-02 12:52:53 +0100

    added non-linear example test

--- adjoint/test_LbfgsPPC.py
+++ adjoint/test_LbfgsPPC.py
@@ -10,12 +10,34 @@ from scipy.integrate import trapz
 from scipy.optimize import minimize
 
 
-def tes1():
+class GeneralPowerEndTermPCP(SimplePpcProblem):
+
+    """
+    class for the opti-problem:
+    J(u,y) = 0.5*||u||**2 + 1/p*(y(T)-yT)**p
+    with y' = ay + u
+    """
+
+    def __init__(self,y0,yT,T,a,power,J,grad_J,options=None):
+        SimplePpcProblem.__init__(self,y0,yT,T,a,J,grad_J,options)
+        self.power = power
+    
+        def J_func(u,y,yT,T):
+            return J(u,y,yT,T,self.power)
+        
+        self.J = J_func
+
+    def initial_adjoint(self,y):
+        
+        p = self.power
+        return (y - self.yT)**(p-1)
+
+def test1():
 
     y0 = 1
     yT = 30
     T  = 1
-    a  = 4
+    a  = 1
     N  = 500
     m  = 10
 
@@ -37,8 +59,8 @@ def tes1():
     
     res1 = problem.solve(N)
 
-    res2 = problem.PPCLBFGSsolve(N,m,[100])
-    res3 = problem.penalty_solve(N,m,[100])
+    res2 = problem.PPCLBFGSsolve(N,m,[10])
+    res3 = problem.penalty_solve(N,m,[10])
     print res2.niter,res3['iteration']
     t = np.linspace(0,T,N+1)
     plt.plot(t,res1['control'].array(),'r--')
@@ -46,5 +68,39 @@ def tes1():
     plt.plot(t,res3['control'].array()[:N+1])
     plt.show()
 
+
+def test2():
+    y0 = 1
+    yT = 10
+    T  = 1
+    a  = 1
+    N  = 500
+    m  = 10
+    p = 4
     
-tes1()
+
+    def J(u,y,yT,T,power):
+        t = np.linspace(0,T,len(u))
+
+        I = trapz(u**2,t)
+
+        return 0.5*I + (1./power)*(y-yT)**power
+
+    def grad_J(u,p,dt):
+        
+        return dt*(u+p)
+
+
+    problem = GeneralPowerEndTermPCP(y0,yT,T,a,p,J,grad_J)
+
+    res1 = problem.solve(N)
+    res2 = problem.PPCLBFGSsolve(N,m,[100])
+    res3 = problem.penalty_solve(N,m,[100])
+    print res1['iteration'],res2.niter,res3['iteration']
+    t = np.linspace(0,T,N+1)
+    plt.plot(t,res1['control'].array(),'r--')
+    plt.plot(t,res2.x[:N+1])
+    plt.show()
+if __name__ == '__main__':
+    #test1()
+    test2()
