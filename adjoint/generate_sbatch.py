1c2145eebb5c500b71ff368aa6227ca6fef98f6d
andrthu <andrthu@student.matnat.uio.no>
2017-05-15 07:53:35 +0200

    small stuff

--- adjoint/generate_sbatch.py
+++ adjoint/generate_sbatch.py
@@ -10,8 +10,19 @@ name = '%s'
 print name
 ''' %(val,name)
 
+string2 = """
+while [ $((10-$COUNTER2)) -gt 0 ]; do
+	mpiexec python func_grad_speedup_test.py %s 1 1 $val;
+	let COUNTER2=COUNTER2+1;
+done
+mpiexec python func_grad_speedup_test.py %s 1 0 $val;
+mpiexec python func_grad_speedup_test.py %s 1 2 $val;
+
+rm temp_time.txt;
+""" %(val,val,val)
+print string2
 new_script = open(name,'w')
 
-print string
+
 new_script.write(string)
 new_script.close()
